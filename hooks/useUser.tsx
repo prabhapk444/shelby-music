4e7d8bafdc6ca1e6972339df3758637bfacf7d83
prabhapk444 <karanprabha22668@gmail.com>
2024-12-01 11:24:37 +0530

    sold

--- hooks/useUser.tsx
+++ hooks/useUser.tsx
@@ -34,7 +34,7 @@ export const MyUserContextProvider = (props: Props) => {
     const [subscription, setSubscription] = useState<Subscription | null>(null);
 
     const getUserDetails = () => supabase.from('users').select('*').single();
-    const getSubscription = () => supabase.from('subscription').select('*,prices(*,products(*))').in('status', ['trialing', 'active']).single();
+    const getSubscription = () => supabase.from('subscriptions').select('*,prices(*,products(*))').in('status', ['trialing', 'active']).single();
 
     useEffect(() => {
         if (user && !isLoadingData && !userDetails && !subscription) {
